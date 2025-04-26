e28576696b90f28e4a380261a22fc37432ffcf97
franovalle <francelie.ovalle@gmail.com>
2025-04-26 16:52:10 -0400

    debugged update section

--- app/routes.js
+++ app/routes.js
@@ -51,41 +51,39 @@ module.exports = function (app, passport, db) {
   })*/
   //using https://zellwk.com/blog/crud-express-mongodb/ as a guide for this project
   //note to self: had to go back and compare it to template given in class 
+  //note to self: need to do more reasearch do get an alert if wrong or right, thinking of making a conditional? 
+  //note to self: I would like the update to do more but for now, it can display the correct answer when ask to check
   //
   app.put('/exam', (req, res) => {
     db.collection('exam')
-      .findOneAndUpdate(
-        { name: 'heart' },
-        {
-          $set: {
-            name: req.body.name,
-            entry: req.body.entry
-
-          }
-        },
-        {
-          upsert: true,
-        },
-        (err, result) => {
-          if (err) return res.send(err)
-          res.send(result)
-        })
-
+    .findOneAndUpdate({answerone: req.body.answerone, answertwo: req.body.answertwo, answerthree: req.body.answerthree }, {
+      $set: {
+        update:req.body.update
+        
+      }
+    }, {
+      /*sort: {_id: -1},*/
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
   })
   //
+
   app.delete('/exam', (req, res) => {
-    db.collection('exam').findOneAndDelete({answerone: req.body.answerone, answertwo: req.body.answertwo, answerthree: req.body.answerthree}, (err, result) => {
+    db.collection('exam').findOneAndDelete({ answerone: req.body.answerone, answertwo: req.body.answertwo, answerthree: req.body.answerthree }, (err, result) => {
       if (err) return res.send(500, err)
       res.send('Answer deleted!')
     })
   })
   //
- /* app.delete('/exam', (req, res) => {
-    db.collection('exam').findOneAndDelete({ name: 'Aaliyah' }, (err, result) =>{
-      if (err) return res.send(500, err)
-        res.send('Deleted Aaliyah quote')
-    }) 
-  })*/
+  /* app.delete('/exam', (req, res) => {
+     db.collection('exam').findOneAndDelete({ name: 'Aaliyah' }, (err, result) =>{
+       if (err) return res.send(500, err)
+         res.send('Deleted Aaliyah quote')
+     }) 
+   })*/
 
   //keeping it here in case above does not work 
   /*app.delete('/verses', (req, res) => {
@@ -194,113 +192,113 @@ function isLoggedIn(req, res, next) {
         res.redirect('/profile')
       })
   }*/
-  /*app.post('/exam', (req, res) => {
-    db.collection('exam')
-      .insertOne(req.body)
-      .then(result => {
-        console.log(result);
-        res.redirect('/profile')
-      })
-      .catch(error => console.log(error))
-  })*/
-
-  /*app.post('/exam', (req, res) => {
-    db.collection('exam').insertOne({ entry: req.body.entry }, (err, result) => {
-      if (err) return console.log(err)
-      console.log('saved to database')
+/*app.post('/exam', (req, res) => {
+  db.collection('exam')
+    .insertOne(req.body)
+    .then(result => {
+      console.log(result);
       res.redirect('/profile')
     })
+    .catch(error => console.log(error))
+})*/
+
+/*app.post('/exam', (req, res) => {
+  db.collection('exam').insertOne({ entry: req.body.entry }, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/profile')
   })
+})
 
-  /*app.put('/exam', (req, res) => {
-    db.collection('exam')
-      .findOneAndUpdate(
-        { entry: 'heart' },
-        {
-          $set: {
-            entry: req.body.entry
-            //answerone: req.body.answerone,
-            //answertwo: req.body.answertwo
-          }
-        },
-        {
-          //sort: {_id: -1},
-          upsert: true
-        },
-        (err, result) => {
-          if (err) return res.send(err)
-          res.send(result)
-        })
-  })*/
+/*app.put('/exam', (req, res) => {
+  db.collection('exam')
+    .findOneAndUpdate(
+      { entry: 'heart' },
+      {
+        $set: {
+          entry: req.body.entry
+          //answerone: req.body.answerone,
+          //answertwo: req.body.answertwo
+        }
+      },
+      {
+        //sort: {_id: -1},
+        upsert: true
+      },
+      (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+})*/
 
-  /*app.delete('/exam', (req, res) => {
-    db.collection('exam').findOneAndDelete({ entry: 'heart' /* answertwo: req.body.answertwo /*answerthree: req.body.answerthree }, (err, result) => {
-      if (err) return res.send(500, err)
-      res.send('Incorrect answer!')
-    })
-  })*/
-
-  /*app.delete('/exam', (req, res) => {
-    db.collection('exam').findOneAndDelete({entry: req.body.entry }, (err, result) => {
-      if (err) return res.send(500, err)
-      res.send('Answer deleted!')
-    })
+/*app.delete('/exam', (req, res) => {
+  db.collection('exam').findOneAndDelete({ entry: 'heart' /* answertwo: req.body.answertwo /*answerthree: req.body.answerthree }, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('Incorrect answer!')
   })
+})*/
 
-  // =============================================================================
-  // AUTHENTICATE (FIRST LOGIN) ==================================================
-  // =============================================================================
+/*app.delete('/exam', (req, res) => {
+  db.collection('exam').findOneAndDelete({entry: req.body.entry }, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('Answer deleted!')
+  })
+})
 
-  // locally --------------------------------
-  // LOGIN ===============================
-  // show the login form
-  app.get('/login', function (req, res) {
-    res.render('login.ejs', { message: req.flash('loginMessage') });
+// =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
+
+// locally --------------------------------
+// LOGIN ===============================
+// show the login form
+app.get('/login', function (req, res) {
+  res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+// process the login form
+app.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/profile', // redirect to the secure profile section
+  failureRedirect: '/login', // redirect back to the signup page if there is an error
+  failureFlash: true // allow flash messages
+}));
+
+// SIGNUP =================================
+// show the signup form
+app.get('/signup', function (req, res) {
+  res.render('signup.ejs', { message: req.flash('signupMessage') });
+});
+
+// process the signup form
+app.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/profile', // redirect to the secure profile section
+  failureRedirect: '/signup', // redirect back to the signup page if there is an error
+  failureFlash: true // allow flash messages
+}));
+
+// =============================================================================
+// UNLINK ACCOUNTS =============================================================
+// =============================================================================
+// used to unlink accounts. for social accounts, just remove the token
+// for local account, remove email and password
+// user account will stay active in case they want to reconnect in the future
+
+// local -----------------------------------
+app.get('/unlink/local', isLoggedIn, function (req, res) {
+  var user = req.user;
+  user.local.email = undefined;
+  user.local.password = undefined;
+  user.save(function (err) {
+    res.redirect('/profile');
   });
-
-  // process the login form
-  app.post('/login', passport.authenticate('local-login', {
-    successRedirect: '/profile', // redirect to the secure profile section
-    failureRedirect: '/login', // redirect back to the signup page if there is an error
-    failureFlash: true // allow flash messages
-  }));
-
-  // SIGNUP =================================
-  // show the signup form
-  app.get('/signup', function (req, res) {
-    res.render('signup.ejs', { message: req.flash('signupMessage') });
-  });
-
-  // process the signup form
-  app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/profile', // redirect to the secure profile section
-    failureRedirect: '/signup', // redirect back to the signup page if there is an error
-    failureFlash: true // allow flash messages
-  }));
-
-  // =============================================================================
-  // UNLINK ACCOUNTS =============================================================
-  // =============================================================================
-  // used to unlink accounts. for social accounts, just remove the token
-  // for local account, remove email and password
-  // user account will stay active in case they want to reconnect in the future
-
-  // local -----------------------------------
-  app.get('/unlink/local', isLoggedIn, function (req, res) {
-    var user = req.user;
-    user.local.email = undefined;
-    user.local.password = undefined;
-    user.save(function (err) {
-      res.redirect('/profile');
-    });
-  });
+});
 
 };
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated())
-    return next();
+if (req.isAuthenticated())
+  return next();
 
-  res.redirect('/');
+res.redirect('/');
 }*/
--- public/main.js
+++ public/main.js
@@ -1,6 +1,33 @@
 //debugging starting over
 var remove = document.getElementsByClassName("fa-times");
+var update = document.getElementsByClassName("fa-check");
 
+
+Array.from(update).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const answerone = this.parentNode.parentNode.childNodes[1].innerText//note to self: I had to do some research on childnodes and parent nodes did not realize the spaces where being counted, need to do more research but it worked after playing around with numbers.  https://stackoverflow.com/questions/65694145/how-to-determine-whether-the-childnodes-has-ul-present-or-not
+    const answertwo = this.parentNode.parentNode.childNodes[5].innerText
+    const answerthree = this.parentNode.parentNode.childNodes[9].innerText
+     
+    fetch('exam', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'answerone': 'brain', 
+        'answertwo': 'brain',
+        'answerthree': 'kidney'
+
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
 
 
 
